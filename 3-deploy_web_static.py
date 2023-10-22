fab30a856fe03decb37fd877548e7501eb1b8ce7
Henok-Haile <henokhaile53@gmail.com>
2023-10-22 03:44:27 -0700

    Task

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# A Fabric script (based on the file 2-do_deploy_web_static.py) that creates
+# and distributes an archive to your web servers, using the function deploy
+
+import os
+from fabric.api import run, env, put
+import time
+env.hosts = ["100.25.164.254", "34.203.201.252"]
+
+
+def do_pack():
+    """Creates a tar gziiped archive of the folder of the web_static."""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        False if the file at the path archive_path doesn’t exist else True.
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("sudo rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("sudo rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
+
+
+def deploy():
+    """Create and distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
