023af551e32ed83f92813dd80e4ff22c62f950cd
Henok-Haile <104774648+Henok-Haile@users.noreply.github.com>
2024-04-18 16:38:16 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # A bash script that sets up your web servers for the deployment of web_static
-sudo apt-get update -y
-sudo apt-get install -y nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "Cohort 13 ALX School student" > /data/web_static/releases/test/index.html
+sudo echo "Cohort 19 ALX School student" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '61 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
