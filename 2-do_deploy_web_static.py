4e624c711ff015f43613f7b685a1dba00346d829
Henok-Haile <henokhaile53@gmail.com>
2024-04-18 08:50:24 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,29 +8,6 @@ from fabric.api import run, env, put
 env.hosts = ["54.198.54.29", "54.165.42.234"]
 
 
-def do_pack():
-    """Creates a tar gziiped archive of the folder of the web_static."""
-    try:
-        if not os.path.exists("web_static"):
-            print("Error: 'web_static' folder does not exist.")
-            return None
-
-        # Create the 'versions' directory if it doesn't exist
-        local("sudo mkdir -p versions")
-
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-
-        # Get the file size
-        file_size = os.path.getsize(file_name)
-
-        print(f"web_static packed: {file_name} -> {file_size}Bytes")
-        return file_name
-    except:
-        return None
-
-
 def do_deploy(archive_path):
     """Distributes an archive to your web servers.
 
