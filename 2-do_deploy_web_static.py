ce45b8ed644ee0943b0ec17e0ffd983f1d0999c9
Henok-Haile <henokhaile53@gmail.com>
2024-04-21 11:22:39 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# A Fabric script (based on the file 1-pack_web_static.py)
-# that distributes an archive to the web servers
+""" A Fabric script (based on the file 1-pack_web_static.py)
+ that distributes an archive to the web servers
+"""
 
+from fabric.api import *
+from datetime import datetime
 import os
-from fabric.api import run, env, put
 
 env.hosts = ["54.198.54.29", "54.165.42.234"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
@@ -16,6 +20,7 @@ def do_deploy(archive_path):
     Returns:
         False if the file at the path archive_path doesn’t exist else True.
     """
+
     if os.path.exists(archive_path) is False:
         return False
 
@@ -25,13 +30,14 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
 
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}'.format(path, archive_name))
-        run('tar -xzf /tmp/{} -C {}{}'.format(archive, path, archive_name))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
-        run('rm -rf {}{}/web_static'.format(path, archive_name))
-        run('rm -rf /data/web_stattic/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, archive_name))
+        run('sudo mkdir -p {}{}'.format(path, archive_name))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(archive, path, archive_name))
+        run('sudo rm /tmp/{}'.format(archive))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
+        run('sudo rm -rf {}{}/web_static'.format(path, archive_name))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, archive_name))
+        print("New version deployed!")
         return True
     except:
-        return false
+        return False
