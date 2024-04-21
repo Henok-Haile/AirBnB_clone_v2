e1a5c1dfdb7e630688b4d9b35b755b15dde5ca65
Henok-Haile <henokhaile53@gmail.com>
2024-04-21 12:36:01 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,17 +13,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split("/")[-1]
-        archive_name = file_n.split(".")[0]
+        file_nm = archive_path.split("/")[-1]
+        arc_nm = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, archive_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, archive_name))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
-        run('rm -rf {}{}/web_static'.format(path, archive_name))
+        run('mkdir -p {}{}/'.format(path, arc_nm))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_nm, path, arc_nm))
+        run('rm /tmp/{}'.format(file_nm))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, arc_nm))
+        run('rm -rf {}{}/web_static'.format(path, arc_nm))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, archive_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, arc_nm))
         print("New version deployed!")
         return True
     except:
