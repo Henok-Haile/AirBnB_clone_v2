aefc3bcd4aacff2dbf8e1ccf7bd4234f3b545e98
Henok-Haile <henokhaile53@gmail.com>
2024-04-21 12:40:47 -0700

    Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -34,30 +34,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        False if the file at the path archive_path doesn’t exist else True.
-    """
-
-    if os.path.exists(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        archive = archive_path.split("/")[-1]
-        archive_name = archive.split(".")[0]
+        file_nm = archive_path.split("/")[-1]
+        arc_nm = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}'.format(path, archive_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(archive, path, archive_name))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
-        run('rm -rf {}{}/web_static'.format(path, archive_name))
-        run('rm -rf /data/web_stattic/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, archive_name))
+        run('mkdir -p {}{}/'.format(path, arc_nm))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_nm, path, arc_nm))
+        run('rm /tmp/{}'.format(file_nm))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, arc_nm))
+        run('rm -rf {}{}/web_static'.format(path, arc_nm))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, arc_nm))
         print("New version deployed!")
         return True
     except:
