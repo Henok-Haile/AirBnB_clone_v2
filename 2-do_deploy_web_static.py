c43e9739d26140ad9fbf02d8eca88a985c1a2036
Henok-Haile <henokhaile53@gmail.com>
2024-05-12 11:27:37 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,18 +14,17 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split("/")[-1]
-        archive_name = file_name.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, archive_name))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, archive_name))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name))
-        run('sudo rm -rf {}{}/web_static'.format(path, archive_name))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, archive_name))
-        print("New version deployed!")
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
